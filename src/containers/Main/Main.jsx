8a371fd3e91ab32dcbd005858c2ec9c45c15b655
Rufo Gabrillo Jr. <rufo.gabrillojr.@escolifesciences.com>
2026-03-12 02:03:37 +0800

    Add datatable

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import DataTable from 'datatables.net-dt';
+
 import Menu from "../../components/DashboardMenu";
 import SideNav from "../../components/SideMenu";
 
@@ -6,7 +8,36 @@ import printing from '../../assets/images/printing.png'
 import search from '../../assets/images/search.png'
 import add from '../../assets/images/add.png'
 
+var data = [
+    {
+        id: "2",
+        name: "product1",
+        price: 100
+    }
+]
+
+
 export default function Main() {
+    const [priceList, setPriceList] = useState(data);
+
+    useEffect( () => {
+        console.log("test")
+        let table = new DataTable('#price-list', {
+            columns: [
+                { title: "ID", data: "id" },
+                { title: "Name", data: "name" },
+                { title: "Price", data: "price" }
+              ],
+            data: priceList,
+            paging: false,
+            searching: false
+        });
+
+        return () => {
+            table.destroy(); // cleanup when component rerenders
+          };
+    }, [priceList] )
+
     return (
         <>
             <Menu />
@@ -14,8 +45,8 @@ export default function Main() {
             <div className="container" style={{ background: "#fff", height: "100%", color: "#000", padding: "0px 0px 0px 210px" }}>
                 <div style={{ padding: "20px 50px", display: "flex", justifyContent: "space-between" }}>
                     <div className="filters" style={{ display: "flex", flexDirection: "column" }}>
-                        <div style={{display: "flex", alignItems: "flex-end"}}><input type="text" placeholder="Search Article No." /><img style={{width: "25px"}} src={search} alt="" /></div>
-                        <div style={{display: "flex", alignItems: "flex-end"}}><input type="text" placeholder="Search Product" /><img style={{width: "25px"}} src={search} alt="" /></div>
+                        <div style={{ display: "flex", alignItems: "flex-end" }}><input type="text" placeholder="Search Article No." /><img style={{ width: "25px" }} src={search} alt="" /></div>
+                        <div style={{ display: "flex", alignItems: "flex-end" }}><input type="text" placeholder="Search Product" /><img style={{ width: "25px" }} src={search} alt="" /></div>
                     </div>
                     <div className="actions" style={{ display: "flex", flexDirection: "row" }}>
                         <button>New Product</button>
@@ -23,7 +54,10 @@ export default function Main() {
                         <button>Advanced Mode</button>
                     </div>
                 </div>
-                <div style={{ padding: "0px 50px" }}>table</div>
+                <div style={{ padding: "0px 50px" }}>
+                    <table  className="display" id="price-list">
+                    </table>
+                </div>
             </div>
         </>
     );
