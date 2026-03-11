bcef39edf5ff1566f7f61b7800fe092126373340
Rufo Gabrillo Jr. <rufo.gabrillojr.@escolifesciences.com>
2026-03-11 19:38:28 +0800

    updates styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -31,34 +31,34 @@ export default function Menu() {
 
     return (
         <div className="menu">
-            <div className="nav">
+            <div style={{display: "flex", justifyContent: "space-between", width: "75%", margin: "0 auto", padding: "2.5rem 0rem"}}>
                 <div className="logo" onClick={toggleMenu}>
-                    <a href="/" className="image"><img src={logo} /></a>
+                    <a href="/"><img src={logo}  style={{height: "2rem"}} /></a>
                     <a href="#" className="icon">
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="navigation-svg" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
                         </svg>
                     </a>
                 </div>
-                <div className={`links ${showMenu ? "show" : ""}`}>
+                <div className={`links ${showMenu ? "show" : ""}`} style={{display: "flex"}}>
                     <a href="#" className="link">Home</a>
                     <a href="#" className="link">Order</a>
                     <a href="#" className="link">Our Customers</a>
                     <a href="#" className="link">About Us</a>
                     <a href="#" className="link">Contact Us</a>
                     <div className="sub-menu" onClick={toggleLanguage}>
-                        <a href="#" className="link drop-down" style={{color: "white", padding: "0px"}}>
+                        <a href="#" className="link drop-down" style={{ color: "white", padding: "0px" }}>
                             English <img src={brit} alt="" className="flag" />
                         </a>
                         {showLanguage && <LanguageSelection />}
                     </div>
                 </div>
-                <div className="sub-menu" onClick={toggleLanguage}>
-                        <a href="#" className="link drop-down" style={{color: "white", padding: "0px"}}>
-                            English <img src={brit} alt="" className="flag" />
-                        </a>
-                        {showLanguage && <LanguageSelection />}
-                    </div>
+                <div className="sub-menu mobile" onClick={toggleLanguage}>
+                    <a href="#" className="link drop-down" style={{ color: "white", padding: "0px" }}>
+                        English <img src={brit} alt="" className="flag" />
+                    </a>
+                    {showLanguage && <LanguageSelection />}
+                </div>
             </div>
         </div>
     );
