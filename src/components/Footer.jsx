5b28ffdbf67fac9d6fb90ffb15252e0dfe376b3d
Rufo Gabrillo Jr. <rufo.gabrillojr.@escolifesciences.com>
2026-03-11 20:03:04 +0800

    update footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 export default function Footer() {
     return (
-        <div className="footer">
-            <div className="section">
+        <div className="footer" style={{bottom: "0px", width: "90%", margin: "0 auto", padding: "10px 0px"}}>
+            <div className="section" style={{display: "flex", justifyContent: "space-between"}}>
                 <div className="logo">
                     <div className="left">123 Fakturera</div>
                 </div>
@@ -13,7 +13,7 @@ export default function Footer() {
                     <a href="#" className="link">Contact Us</a>
                 </div>
             </div>
-            <div className="copy-right"><p>© Lättfaktura, CRO no. 638537, 2025. All rights reserved. </p></div>
+            <div className="copy-right" style={{width: "100%"}}><p style={{textAlign: "center"}}>© Lättfaktura, CRO no. 638537, 2025. All rights reserved. </p></div>
         </div>
     );
 }
